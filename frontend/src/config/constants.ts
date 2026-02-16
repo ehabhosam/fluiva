3a4db1dd27fa22e1aec30212b48226512453991c
ehabhosam <ehab.hosam2019@gmail.com>
2026-02-16 14:22:54 +0200

    remove unwanted constatns logs

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,17 +1,3 @@
-console.log("[frontend constants]: Final values:");
-console.log(
-  "[frontend constants]: VITE_API_URL =",
-  import.meta.env.VITE_API_URL,
-);
-console.log(
-  "[frontend constants]: VITE_SUPABASE_URL =",
-  import.meta.env.VITE_SUPABASE_URL,
-);
-console.log(
-  "[frontend constants]: VITE_SUPABASE_ANON_KEY =",
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-);
-
 // export const VITE_API_URL =
 //   import.meta.env.VITE_API_URL || "http://localhost:3000";
 export const VITE_API_URL =
