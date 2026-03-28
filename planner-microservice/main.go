104856bb2a2bc6844ef1530fd68891981caaed0b
ehabhosam <ehab.hosam2019@gmail.com>
2026-03-28 13:33:23 +0200

    chore(planner-microservice): remove unwanted worker pool impl & its dependencies

--- planner-microservice/main.go
+++ planner-microservice/main.go
@@ -1,26 +1,3 @@
-// package main
-
-// import (
-// 	"planner-microservice/handler"
-// 	"planner-microservice/planner"
-
-// 	"github.com/gofiber/fiber/v2"
-// )
-
-// func main() {
-// 	planner.TestSamplePlanner()
-
-// 	app := fiber.New()
-
-// 	// Start worker pool
-// 	handler.InitWorkerPool(10) // 10 concurrent workers
-
-// 	// Job endpoint
-// 	app.Post("/job", handler.HandleJob)
-
-// 	app.Listen(":3000")
-// }
-
 package main
 
 import (
